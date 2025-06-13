684237bba2362b5fa0d8bc9bb122abfeee876d09
Ivan G. Irizarry <cristophergiovanni@gmail.com>
2025-06-12 23:44:34 -0500

    Update main-view.jsx
    removed hard-coded data, used 'fetch' to get API data

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,5 +1,5 @@
 // Importing UseState
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 // Importing MovieCard
 import { MovieCard } from "../movie-card/movie-card";
@@ -9,37 +9,26 @@ import { MovieView } from "../movie-view/movie-view";
 
 
 export const MainView = () => {
-  const [movies, setMovies] = useState([
-    {
-        id: 1, 
-        title: "Enter the Dragon",
-        description: "A Shaolin martial artist travels to an island fortress to spy on an opium lord - who is also a former monk from his temple - under the guise of attending a fighting tournament.", 
-        genre: "Action", 
-        director: "Robert Clouse", 
-        actor: "Bruce Lee",
-        image: "https://m.media-amazon.com/images/M/MV5BZDAyYjAwNmMtMjM5OC00NTc5LThmNjAtOTUxODQxOWYwOTE5XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg"
-    },
-    {
-        id: 2, 
-        title: "Drunken Master",
-        description: "Wong Fei-Hung is a mischievous, yet righteous young man, but after a series of incidents, his frustrated father has him disciplined by a master of drunken martial arts.", 
-        genre: "Action", 
-        director: "Yuen Woo-ping", 
-        actor: "Jackie Chan",
-        image: "https://i.ebayimg.com/images/g/OPQAAOSwNSxVTlnE/s-l1200.jpg"
-    },
-    {
-        id: 3, 
-        title: "Bloodsport",
-        description: '"Bloodsport" follows Frank Dux, an American martial artist serving in the military, who decides to leave the army to compete in a martial arts tournament in Hong Kong where fights to the death can occur.', 
-        genre: "Action", 
-        director: "Newt Arnold", 
-        actor: "Jean Claude Van Damme",
-        image: "https://m.media-amazon.com/images/M/MV5BMTQwOTgzNjkzMl5BMl5BanBnXkFtZTcwMjgxNDcyNA@@._V1_.jpg"
-    }
-  ]);
+  const [movies, setMovies] = useState([]);
 
   const [selectedMovie, setSelectedMovie] = useState(null);
+
+  useEffect(() => {
+    fetch("")
+      .then((response) => response.json())
+      .then((data) => {
+        const moviesFromApi = data.docs.map((doc) => {
+          return {
+            id: doc.key,
+            title: doc.title,
+            image: ``,
+            director: doc.director_name?.[0]
+          };
+        });
+
+        setMovies(moviesFromApi);
+      });
+  }, []);
 
     if (selectedMovie) {
         return (
