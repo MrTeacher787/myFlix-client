71d1ccf7f0c16302729a8cad74a519a9cce775b5
Ivan G. Irizarry <cristophergiovanni@gmail.com>
2025-06-17 14:32:35 -0500

    Update movie-card.jsx

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -17,9 +17,10 @@ export const MovieCard = ({ movie, onMovieClick }) => {
 // All props constraints for the MovieCard defined
 MovieCard.propTypes = {
   movie: PropTypes.shape({
+    _id: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
-    image: PropTypes.string.isRequired,
-    director: PropTypes.string,
+    description: PropTypes.string.isRequired,
+    director: PropTypes.string.isRequired,
   }).isRequired,
   onMovieClick: PropTypes.func.isRequired,
 };
