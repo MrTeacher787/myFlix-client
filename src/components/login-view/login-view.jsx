1c97075c16c802ce51d5bf789fa34dbc0a55881c
Ivan G. Irizarry <cristophergiovanni@gmail.com>
2025-07-08 18:39:24 -0500

    Update login-view.jsx

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,15 +1,52 @@
-export const LoginView = () => {
-    return (
-      <form>
-        <label>
-            Username:
-            <input type="text" />
-        </label>
-        <label>
-            Password:
-            <input type="password" />
-        </label>
-        <button type="submit">Submit</button>
-      </form>
-    );
+import React from "react";
+
+export const LoginView = ({ onLoggedIn }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event) => {
+    // prevents default behavior of the form
+    event.preventDefault();
+
+    const data = {
+      access: username,
+      secret: password,
+    };
+
+    fetch("https://kickflix-7d36cfc627dc.herokuapp.com/login.json", {
+      method: "POST",
+      body: JSON.stringify(data),
+    }).then((response) => {
+      if (response.ok) {
+        onLoggedIn(username);
+      } else {
+        alert("Login failed!");
+      }
+    });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Username:
+        <input
+          type="text"
+          value={username}
+          minLength={5}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+        />
+      </label>
+      <label>
+        Password:
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </label>
+      <button type="submit">Submit</button>
+    </form>
+  );
 };
