b2ea391da7af3b8164211ae0af1d023bd19413cc
Ivan G. Irizarry <cristophergiovanni@gmail.com>
2025-07-27 21:22:59 -0500

    Update movie-card.jsx

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,18 +1,22 @@
 // Importing PropTypes library
 import PropTypes from "prop-types";
 import { Button, Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 // The MovieCard function component
-export const MovieCard = ({ movie, onMovieClick }) => {
-    return (
-      <Card onClick={() => onMovieClick(movie)} className="h-100">
-        <Card.Img variant="top" src={movie.ImageURL} />
-        <Card.Body style={{ backgroundColor:"white" }}>
-          <Card.Title>{movie.Title}</Card.Title>
-          <Card.Text>{movie.Description}</Card.Text>
-        </Card.Body>
-      </Card>
-    );
+export const MovieCard = ({ movie }) => {
+  return (
+    <Card>
+      <Card.Img variant="top" src={movie.ImageURL} />
+      <Card.Body style={{ backgroundColor:"white" }}>
+        <Card.Title>{movie.Title}</Card.Title>
+        <Card.Text>{movie.Description}</Card.Text>
+        <Link to={`/movies/${encodedURIComponent(movie._id)}`}>
+          <Button variant="link">Open</Button>
+        </Link>
+      </Card.Body>
+    </Card>
+  );
 };
 
 // All props constraints for the MovieCard defined
@@ -24,5 +28,4 @@ MovieCard.propTypes = {
     Director: PropTypes.string.isRequired,
     ImageURL: PropTypes.string.isRequired,
   }).isRequired,
-  onMovieClick: PropTypes.func.isRequired,
 };
