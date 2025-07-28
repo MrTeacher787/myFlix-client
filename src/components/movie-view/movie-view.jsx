a649e3affde74b6cbcd5415d7da39a4aaf014bb3
Ivan G. Irizarry <cristophergiovanni@gmail.com>
2025-07-27 21:23:05 -0500

    Update movie-view.jsx

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,50 +1,39 @@
 import PropTypes from "prop-types";
 import Button from "react-bootstrap/Button";
+import { useParams } from "react-router";
+import { Link } from "react-router-dom";
 
-export const MovieView = ({ movie, onBackClick }) => {
-    return (
-        <div className="name-container">
-                <img className="w-100" src={movie.ImageURL} />                
-            <div>
-                <span>Title: </span>
-                <span>{movie.Title}</span>
-            </div>
-            <div>
-                <span>Description: </span>
-                <span>{movie.Description}</span>
-            </div>
-            <div>
-                <span>Genre: </span>
-                <span>{movie.Genre.Name}</span>
-            </div>
-            <div>
-                <span>Director: </span>
-                <span>{movie.Director.Name}</span>
-            </div>
-            <div>
-                <span>Actor: </span>
-                <span>{movie.Actor[0].Name}</span>
-            </div>
-            <Button 
-              onClick={onBackClick}
-              className="button"
-              style={{ cursor: "pointer" }}
-            >
-              Back
-            </Button>
-        </div>
-    );
-};
+export const MovieView = ({ movies }) => {
+  const { movieId } = useParams();
 
-MovieView.propTypes = {
-  movie: PropTypes.shape({
-    _id: PropTypes.string.isRequired,
-    Title: PropTypes.string.isRequired,
-    Actor: PropTypes.string.isRequired,
-    Genre: PropTypes.string.isRequired,
-    Description: PropTypes.string.isRequired,
-    Director: PropTypes.string.isRequired,
-    ImageURL: PropTypes.string.isRequired,
-  }).isRequired,
-  onBackClick: PropTypes.func.isRequired,
+  const movie = movies.find((m) => m._id === movieId);
+    
+  return (
+    <div className="name-container">
+      <img className="w-100" src={movie.ImageURL} />                
+      <div>
+        <span>Title: </span>
+        <span>{movie.Title}</span>
+      </div>
+      <div>
+        <span>Description: </span>
+        <span>{movie.Description}</span>
+      </div>
+      <div>
+        <span>Genre: </span>
+        <span>{movie.Genre.Name}</span>
+      </div>
+      <div>
+        <span>Director: </span>
+        <span>{movie.Director.Name}</span>
+      </div>
+      <div>
+        <span>Actor: </span>
+        <span>{movie.Actor[0].Name}</span>
+      </div>
+      <Link to={`/`}>
+        <button className="button">Back</button>
+      </Link>
+    </div>
+  );
 };
