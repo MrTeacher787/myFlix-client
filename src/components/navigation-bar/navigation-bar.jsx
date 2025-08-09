57290b7c4f5d5c2ccf71f3870fd94da6b37aa6a7
Ivan G. Irizarry <cristophergiovanni@gmail.com>
2025-08-08 20:02:18 -0500

    Update navigation-bar.jsx

--- src/components/navigation-bar/navigation-bar.jsx
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -16,7 +16,7 @@ export const NavigationBar = ({ user, onLoggedOut }) => {
                 <Nav.Link as={Link} to="/login">
                   Login
                 </Nav.Link>
-                <Nav.Link as={Link} to="/users">
+                <Nav.Link as={Link} to="/signup">
                   Signup
                 </Nav.Link>
               </>
@@ -26,7 +26,7 @@ export const NavigationBar = ({ user, onLoggedOut }) => {
                 <Nav.Link as={Link} to="/">
                   Home
                 </Nav.Link>
-                <Nav.Link as={Link} to="/users">
+                <Nav.Link as={Link} to="/profile">
                   Profile
                 </Nav.Link>
                 <Nav.Link onClick={onLoggedOut}>
